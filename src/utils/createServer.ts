b99ccd3e3749e8199fe49ba0286d5addef1871df
Mark Bran <47259119+markytobran@users.noreply.github.com>
2022-11-19 23:06:02 +0000

    add cors

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,9 +1,10 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { videosRoute } from '../modules/video/video.route'
 
 export async function createServer() {
   const app = fastify()
-
+  app.register(cors)
   app.register(videosRoute, { prefix: '/api/videos' })
   return app
 }
