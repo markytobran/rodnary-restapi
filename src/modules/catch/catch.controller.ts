c5899d3185cfc3d7b16e4746d6dfa0bb70c3769a
Mark Bran <47259119+markytobran@users.noreply.github.com>
2023-08-20 19:49:08 +0100

    feat: add catches handler

--- src/modules/catch/catch.controller.ts
+++ src/modules/catch/catch.controller.ts
@@ -1,7 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { logger } from '../../utils/logger'
 import { CreateCatchBody } from './catch.schema'
-import { createCatch } from './catch.service'
+import { getCatches, createCatch } from './catch.service'
+
+export async function getCatchesHandler(req: FastifyRequest, reply: FastifyReply) {
+  try {
+    const videos = await getCatches()
+    return reply.code(200).send(videos)
+  } catch (e) {
+    logger.error(e, 'getCatchesHandler: error getting catches')
+    return reply.code(500).send({ message: 'Error getting catches' })
+  }
+}
 
 export async function createCatchHandler(req: FastifyRequest<{ Body: CreateCatchBody }>, reply: FastifyReply) {
   try {
--- src/modules/catch/catch.route.ts
+++ src/modules/catch/catch.route.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
-import { createCatchHandler } from './catch.controller'
+import { createCatchHandler, getCatchesHandler } from './catch.controller'
 import { createCatchSchema } from './catch.schema'
 
 export function catchRoute(app: FastifyInstance, options: FastifyPluginOptions, done: () => void) {
+  app.get('/', getCatchesHandler)
   app.post('/', { schema: createCatchSchema }, createCatchHandler)
-
   done()
 }
